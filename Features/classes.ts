990e910272ed7f6545241632113fe6b85c53b505
Piotr Kozłowski <pitrx77@gmail.com>
2021-08-03 22:46:40 +0200

    update 3
    Classes in Typescript

---
+++ Features/classes.ts
@@ -0,0 +1,36 @@
+class Vehicle {
+  
+  
+
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log('beep');
+  }
+
+
+}
+
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+
+class Car  extends Vehicle {
+
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log("wroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(20, 'red');
+car.startDrivingProcess()
