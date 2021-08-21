5002f626b4d7cfb3107b9b119f0f91cdb5a31dd6
Piotr Kozłowski <pitrx77@gmail.com>
2021-08-21 23:31:13 +0200

    update 21
    Typescript with react and Redux part2

---
+++ my-app/src/actions/index.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import {Dispatch } from 'redux';
+import {ActionTypes} from '../actions/types'
+
+
+export interface Todo {
+  id: number;
+  title: string;
+  compleated: boolean
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[]
+}
+
+const url = 'http://jsonplaceholder.typicode.com/dodos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data
+    });
+  }
+}
